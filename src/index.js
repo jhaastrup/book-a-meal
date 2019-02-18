8d2eb68fb293286c7c243e0ac4671db72e07eee7
jhaastrup <haastrup21@gmail.com>
2019-02-18 11:45:25 +0100

    Create signup route

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import router from './routes';
 
 const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use('/api/v1', router);
 
 const port = process.env.PORT || 5160;
 
